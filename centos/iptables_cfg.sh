1d652316f87fbbbbec1212e0d7de55c85b522b18
Peter Ajamian <peter@pajamian.dhs.org>
2009-08-19 20:59:49 -0700

    Hard code location of iptables in case it's not in the path.

--- centos/iptables_cfg.sh
+++ centos/iptables_cfg.sh
@@ -1,50 +1,53 @@
 #!/bin/sh
 
 #
-# iptables example configuration script
+# iptables example configuration script must be run as sudo or root.
 #
+
+# Location of iptables binary
+IPTABLES=/sbin/iptables
 
 /etc/init.d/iptables start
 
 # Flush all current rules from iptables
 #
- iptables -F
+ $IPTABLES -F
 #
 # Set default policies for INPUT, FORWARD and OUTPUT chains
 #
- iptables -P INPUT DROP
- iptables -P FORWARD DROP
- iptables -P OUTPUT ACCEPT
+ $IPTABLES -P INPUT DROP
+ $IPTABLES -P FORWARD DROP
+ $IPTABLES -P OUTPUT ACCEPT
 #
 # Set access for localhost
 #
- iptables -A INPUT -i lo -j ACCEPT
+ $IPTABLES -A INPUT -i lo -j ACCEPT
 #
 # Accept packets belonging to established and related connections
 #
- iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+ $IPTABLES -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 # Accept pings
- iptables -A INPUT -p icmp -j ACCEPT
+ $IPTABLES -A INPUT -p icmp -j ACCEPT
 
 # Accept tcp packets on destination port 22 (SSH)
- iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+ $IPTABLES -A INPUT -p tcp --dport 22 -j ACCEPT
 
 # Accept tcp and udp packets for destination port 53 (DNS)
-# iptables -A INPUT -p tcp --dport 53 -j ACCEPT
-# iptables -A INPUT -p udp --dport 53 -j ACCEPT
+# $IPTABLES -A INPUT -p tcp --dport 53 -j ACCEPT
+# $IPTABLES -A INPUT -p udp --dport 53 -j ACCEPT
 
 # Accept tcp packets for destination ports 80 and 443 (HTTP[S])
- iptables -A INPUT -p tcp --dport 80 -j ACCEPT
- iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+ $IPTABLES -A INPUT -p tcp --dport 80 -j ACCEPT
+ $IPTABLES -A INPUT -p tcp --dport 443 -j ACCEPT
 
 # Accept tcp packets for destination ports 110 and 995 (POP3[S])
-# iptables -A INPUT -p tcp --dport 110 -j ACCEPT
-# iptables -A INPUT -p tcp --dport 995 -j ACCEPT
+# $IPTABLES -A INPUT -p tcp --dport 110 -j ACCEPT
+# $IPTABLES -A INPUT -p tcp --dport 995 -j ACCEPT
 
 # Accept tcp packets for destination ports 25 and 465 (SMTP[S])
-# iptables -A INPUT -p tcp --dport 25 -j ACCEPT
-# iptables -A INPUT -p tcp --dport 465 -j ACCEPT
+# $IPTABLES -A INPUT -p tcp --dport 25 -j ACCEPT
+# $IPTABLES -A INPUT -p tcp --dport 465 -j ACCEPT
 
 #
 # Save settings
@@ -53,5 +56,4 @@
 #
 # List rules
 #
- iptables -L -v
-
+ $IPTABLES -L -v
