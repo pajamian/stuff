0aef5bf2a27c78339fec22914554216d08250589
Peter Ajamian <peter@pajamian.dhs.org>
2009-08-24 00:26:44 -0700

    Explicitly check that the state is NEW on new connections.
    Explicitly check that the state is NEW on new connections to avoid attacks which
    use invalid packets.

--- centos/iptables_cfg.sh
+++ centos/iptables_cfg.sh
@@ -28,26 +28,26 @@ IPTABLES=/sbin/iptables
  $IPTABLES -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 # Accept pings
- $IPTABLES -A INPUT -p icmp -j ACCEPT
+ $IPTABLES -A INPUT -p icmp -m state --state NEW -j ACCEPT
 
 # Accept tcp packets on destination port 22 (SSH)
- $IPTABLES -A INPUT -p tcp --dport 22 -j ACCEPT
+ $IPTABLES -A INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT
 
 # Accept tcp and udp packets for destination port 53 (DNS)
-# $IPTABLES -A INPUT -p tcp --dport 53 -j ACCEPT
-# $IPTABLES -A INPUT -p udp --dport 53 -j ACCEPT
+# $IPTABLES -A INPUT -p tcp --dport 53 -m state --state NEW -j ACCEPT
+# $IPTABLES -A INPUT -p udp --dport 53 -m state --state NEW -j ACCEPT
 
 # Accept tcp packets for destination ports 80 and 443 (HTTP[S])
- $IPTABLES -A INPUT -p tcp --dport 80 -j ACCEPT
- $IPTABLES -A INPUT -p tcp --dport 443 -j ACCEPT
+ $IPTABLES -A INPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
+ $IPTABLES -A INPUT -p tcp --dport 443 -m state --state NEW -j ACCEPT
 
 # Accept tcp packets for destination ports 110 and 995 (POP3[S])
-# $IPTABLES -A INPUT -p tcp --dport 110 -j ACCEPT
-# $IPTABLES -A INPUT -p tcp --dport 995 -j ACCEPT
+# $IPTABLES -A INPUT -p tcp --dport 110 -m state --state NEW -j ACCEPT
+# $IPTABLES -A INPUT -p tcp --dport 995 -m state --state NEW -j ACCEPT
 
 # Accept tcp packets for destination ports 25 and 465 (SMTP[S])
-# $IPTABLES -A INPUT -p tcp --dport 25 -j ACCEPT
-# $IPTABLES -A INPUT -p tcp --dport 465 -j ACCEPT
+# $IPTABLES -A INPUT -p tcp --dport 25 -m state --state NEW -j ACCEPT
+# $IPTABLES -A INPUT -p tcp --dport 465 -m state --state NEW -j ACCEPT
 
 #
 # Save settings
